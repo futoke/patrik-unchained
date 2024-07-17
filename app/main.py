056da57c97328f2708d562a09955b3b739bccfac
futoke <futoke@gmail.com>
2024-07-17 22:30:43 +0000

    upd

--- app/main.py
+++ app/main.py
@@ -87,18 +87,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-def read_root():
-    return FileResponse('static/index.html')
-
-
-@app.get("/cmd/{cmd_name}/")
-async def do_cmd(cmd_name: str):
-    logger.info(f"Action '{cmd_name}' received.")
-    await queue.put(cmd_name)
+@app.get("/do_action/{action_name}/")
+async def do_action(action_name: str):
+    logger.info(f"Action '{action_name}' received.")
+    await queue.put(action_name)
     return
 
 
@@ -109,71 +103,4 @@ def get_all_actions() -> list[str]:
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
-
-    # ctrl = ServoController(serial.Serial("/dev/ttyUSB0", 115200, timeout=1))
-
-    # while True:
-    #     ctrl.move_prepare(1, 400, 500)
-    #     ctrl.move_prepare(2, 400, 500)
-
-    #     ctrl.move_start()
-
-    #     time.sleep(0.5)
-
-    #     ctrl.move_prepare(1, 480, 500)
-    #     ctrl.move_prepare(2, 480, 500)
-
-    #     ctrl.move_start()
-
-    #     time.sleep(0.5)
-
-    # from lx16a import *
-
-    # LX16A.initialize("/dev/ttyUSB0", 0.1)
-
-    # try:
-    #     servo1 = LX16A(1)
-    #     servo2 = LX16A(2)
-    #     # servo1.set_angle_limits(0, 240)
-    #     # servo2.set_angle_limits(0, 240)
-    # except ServoTimeoutError as e:
-    #     print(f"Servo {e.id_} is not responding. Exiting...")
-    #     quit()
-
-    # t = 0
-    # while True:
-    #     try:
-    #         servo1.move(sin(t) * 60 + 100, time=500, wait=True)
-    #         servo2.move(cos(t) * 60 + 100, time=500, wait=True)
-    #         servo1.move_start()
-
-    #         time.sleep(0.5)
-    #         t += 0.1
-    #     except ServoArgumentError:
-    #         pass
-    
-    # while True:
-    #     try:
-    #         servo1.move(120, time=500)
-    #         servo2.move(25, time=500)
-
-    #         time.sleep(0.5)
-
-    #         servo1.move(130, time=500)
-    #         servo2.move(100, time=500)
-
-    #         time.sleep(0.5)
-
-    #         servo1.move(140, time=500)
-    #         servo2.move(190, time=500)
-
-    #         time.sleep(0.5)
-
-    #         servo1.move(130, time=500)
-    #         servo2.move(100, time=500)
-
-    #         time.sleep(0.5)
-    #     except ServoArgumentError:
-    #         pass
